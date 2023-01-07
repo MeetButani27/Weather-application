8e8fb3f4e141560f3f11cd69a67ae181370fe4a8
MeetButani27 <20CS010@charusat.edu.in>
2023-01-07 11:11:24 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WeatherWidgetComponent } from './component/weather-widget/weather-widget.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherWidgetComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
